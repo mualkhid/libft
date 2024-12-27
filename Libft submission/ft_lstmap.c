4fadff035d486117995b60aef2aee481b1c94c06
mualkhid <162097353+mualkhid@users.noreply.github.com>
2024-12-27 14:57:30 +0400

    Update ft_lstmap.c

--- Libft submission/ft_lstmap.c
+++ Libft submission/ft_lstmap.c
@@ -35,3 +35,25 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	}
 	return (head);
 }
+
+// void *offsetContent(void *content)
+// {
+// 	content = content + 2;
+// 	return(content);
+// }
+// int main(void)
+// {
+// 	t_list *node1 = ft_lstnew(ft_strdup("bruh"));
+// 	t_list *node2 = ft_lstnew(ft_strdup("zxda"));
+// 	t_list *node3 = ft_lstnew(ft_strdup("qweqwe"));
+
+// 	ft_lstadd_back(&node1, node2);
+// 	ft_lstadd_back(&node1, node3);
+
+// 	t_list *new = ft_lstmap(node1, offsetContent, free);
+// 	while(new)
+// 	{
+// 		printf("%s\n", new->content);
+// 		new = new->next;
+// 	}
+// }
