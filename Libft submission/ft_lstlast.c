83dc249f261bff6c1688164ad9c0a36eebf75704
mualkhid <162097353+mualkhid@users.noreply.github.com>
2024-12-27 14:57:06 +0400

    Update ft_lstlast.c

--- Libft submission/ft_lstlast.c
+++ Libft submission/ft_lstlast.c
@@ -22,3 +22,16 @@ t_list	*ft_lstlast(t_list *lst)
 	}
 	return (lst);
 }
+
+// int main(void)
+// {
+// 	t_list *node1 = ft_lstnew("bruh");
+// 	t_list *node2 = ft_lstnew("zxda");
+// 	t_list *node3 = ft_lstnew("qweqwe");
+
+// 	ft_lstadd_back(&node1, node2);
+// 	ft_lstadd_back(&node1, node3);
+
+// 	t_list *last_node = ft_lstlast(node1);
+// 	printf("%s\n", last_node->content);
+// }
