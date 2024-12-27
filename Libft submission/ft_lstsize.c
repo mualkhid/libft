d16fd464856f48256fe45697258791bcf7c5fb25
mualkhid <162097353+mualkhid@users.noreply.github.com>
2024-12-27 14:58:35 +0400

    Update ft_lstsize.c

--- Libft submission/ft_lstsize.c
+++ Libft submission/ft_lstsize.c
@@ -26,3 +26,15 @@ int	ft_lstsize(t_list *lst)
 	}
 	return (i);
 }
+
+// int main(void)
+// {
+// 	t_list *node1 = ft_lstnew("ff");
+// 	t_list *node2 = ft_lstnew("zxdffdda");
+// 	t_list *node3 = ft_lstnew("qwfdgfgeqwe");
+
+// 	ft_lstadd_back(&node1, node2);
+// 	ft_lstadd_back(&node1, node3);
+
+// 	printf("%d\n", ft_lstsize(node1));
+// }
